bf36d593c683c58660bd90ec1e3572ef76ef096f
mms-gianni <gianni.carafa@srf.ch>
2018-03-16 11:27:27 +0100

    new Datadog repo key A2923DFF56EDA6E76E55E492D3A80E30382E94DE

--- manifests/ubuntu.pp
+++ manifests/ubuntu.pp
@@ -15,7 +15,7 @@
 class datadog_agent::ubuntu(
   $apt_key = 'A2923DFF56EDA6E76E55E492D3A80E30382E94DE',
   $agent_version = 'latest',
-  $other_keys = ['935F5A436A5A6E8788F0765B226AE980C7A7DA52'],
+  $other_keys = ['A2923DFF56EDA6E76E55E492D3A80E30382E94DE'],
   $location = 'https://apt.datadoghq.com',
   $release = 'stable',
   $repos = 'main',
