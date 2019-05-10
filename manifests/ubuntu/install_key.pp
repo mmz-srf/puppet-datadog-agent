07abfeedd37b60fdd7aaf1ea40f7f087135a2a3a
Bruno Mazenauer <bruno.mazenauer@srf.ch>
2019-05-10 07:56:04 +0200

    manage key with apt::key

--- manifests/ubuntu/install_key.pp
+++ manifests/ubuntu/install_key.pp
@@ -13,10 +13,12 @@
 #
 #
 define datadog_agent::ubuntu::install_key() {
-  $shortkey =  regsubst($name, '.*(.{8})$', '\1')
 
-  exec { "key ${name}":
-    command => "/usr/bin/apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ${name}",
-    unless  => "/usr/bin/apt-key list | grep ${shortkey} | grep expires",
+  apt::key { 'datadog_agent-key':
+    ensure      => 'present',
+    id          => $name,
+    server      => 'hkp://keyserver.ubuntu.com:80', # SRG network does not permit access to x.x.x.x:11371
+    notify      => Class['::apt::update'],
   }
+
 }
